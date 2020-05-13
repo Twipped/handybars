a4d63ac3c8c74862d9b365b38e7dca4feaccea92
Jocelyn Badgley (Twipped) <joc@twipped.com>
2020-05-13 11:43:35 -0700

    Add helper aliases for unless and lookup

--- src/helpers.js
+++ src/helpers.js
@@ -10,6 +10,7 @@ import {
 	any,
 	with as _with,
 	each,
+	get,
 } from './kit';
 
 const helpers = {
@@ -18,11 +19,14 @@ const helpers = {
 	each,
 	log,
 	not,
+	unless: not,
 	has,
 	is,
 	isNot,
 	all,
 	any,
+	get,
+	lookup: get,
 };
 
 export default helpers;
